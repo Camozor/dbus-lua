2256bbc41ad194be7920890a0e71b4f810dc93c6
Camille Vauchel <camille.vauchel@gmail.com>
2026-05-05 19:23:46 +0200

    Revert pause

--- pause.lua
+++ pause.lua
@@ -1,15 +1,11 @@
--- local Dbus = require("dbus-lua.dbus")
---
--- local dbus = Dbus:new()
--- assert(dbus:connect())
---
--- dbus:call_method({
--- 	path = "/org/mpris/MediaPlayer2",
--- 	interface = "org.mpris.MediaPlayer2.Player",
--- 	member = "Pause",
--- 	destination = "org.mpris.MediaPlayer2.spotify",
--- })
+local Dbus = require("dbus-lua.dbus")
 
-local an_array = { 1, 3, 5 }
+local dbus = Dbus:new()
+assert(dbus:connect())
 
-print(type(an_array))
+dbus:call_method({
+	path = "/org/mpris/MediaPlayer2",
+	interface = "org.mpris.MediaPlayer2.Player",
+	member = "Pause",
+	destination = "org.mpris.MediaPlayer2.spotify",
+})
