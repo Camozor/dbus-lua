58db38c1d95050f4138e032053125755b72627c2
Camille <camille.vauchel@gmail.com>
2026-05-09 00:51:12 +0200

    pause play

--- pause.lua
+++ pause.lua
@@ -3,9 +3,20 @@ local Dbus = require("dbus-lua.dbus")
 local dbus = Dbus:new()
 assert(dbus:connect())
 
+print("PAUSE")
 dbus:call_method({
 	path = "/org/mpris/MediaPlayer2",
 	interface = "org.mpris.MediaPlayer2.Player",
 	member = "Pause",
+	destination = "org.mpris.MediaPlayer2.spotify",
+})
+
+os.execute("sleep 1")
+
+print("PLAY")
+dbus:call_method({
+	path = "/org/mpris/MediaPlayer2",
+	interface = "org.mpris.MediaPlayer2.Player",
+	member = "Play",
 	destination = "org.mpris.MediaPlayer2.spotify",
 })
